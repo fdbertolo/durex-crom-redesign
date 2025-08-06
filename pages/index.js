5b009ec57b8af22f6095b851e868f78a4bc61de2
fdbertolo <fdbertolo@gmail.com>
2025-08-06 12:31:18 -0300

    Add new SVG assets for icons and logos
    - Added durexcrom.svg for a new icon design.
    - Added favicon.svg for the website's favicon.
    - Added logo-h.svg for the horizontal logo variant.
    - Added logo-v.svg for the vertical logo variant.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,66 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import MetallicPaint, { parseLogoImage } from "@components/MetallicPaint";
+import { useState, useEffect } from "react";
+
+// Remueve la importación de 'logo'
+// import logo from "../public/durexcrom.svg";
 
 export default function Home() {
+  const [imageData, setImageData] = useState(null);
+
+  useEffect(() => {
+    // Este código solo se ejecutará en el cliente
+    const initialImageData = new ImageData(1, 1);
+    setImageData(initialImageData);
+
+    async function loadDefaultImage() {
+      try {
+        // Usa la ruta pública directa para el archivo
+        const response = await fetch("/durexcrom.svg");
+        const blob = await response.blob();
+        const file = new File([blob], "default.svg", { type: "image/svg+xml" });
+
+        const parsedData = await parseLogoImage(file);
+        setImageData(parsedData?.imageData ?? initialImageData);
+      } catch (err) {
+        console.error("Error loading default image:", err);
+      }
+    }
+
+    loadDefaultImage();
+  }, []);
+
+  if (!imageData) {
+    return null; // O un indicador de carga mientras se inicializa
+  }
+
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Durex Crom</title>
+        <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Durex Crom" />
+        <div style={{ width: "100%", height: "50vh" }}>
+          <MetallicPaint
+            imageData={imageData}
+            params={{
+              edge: 0.1,
+              patternBlur: 0.005,
+              patternScale: 2,
+              refraction: 0.015,
+              speed: 0.3,
+              liquid: 0.07,
+            }}
+          />
+        </div>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
