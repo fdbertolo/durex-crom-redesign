285718fb8976c1220c5770bee5a434991a862b74
fdbertolo <fdbertolo@gmail.com>
2025-09-03 14:47:41 -0300

    fixing category

--- components/ApplicationsSection.jsx
+++ components/ApplicationsSection.jsx
@@ -55,7 +55,7 @@ export function ApplicationsSection() {
 
   const galleryImages = [
     { src: "./works/hostias.png", title: "Molde para hostias", category: "Cromo duro" },
-    { src: "./works/gastro-rodillo.png", title: "Rodillo gastronómico", category: "Cromo duro" },
+    { src: "./works/gastro-rodillo.png", title: "Rodillo gastronómico", category: "Cromo duro + Rectificado" },
     { src: "./works/rolo-cracker.png", title: "Rolo Cracker", category: "Cromo duro + Rectificado" },
     { src: "./works/hidra-rtk.png", title: "RTK Hidráulico", category: "Rectificado" },
     { src: "./works/rotor-picadora-agro.png", title: "Rotor de picadora agrario", category: "Cromo duro" },
