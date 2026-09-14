1315d494c24e42652e1571cb015eab533c8dda59
fdbertolo <fdbertolo@gmail.com>
2026-09-14 11:16:45 -0300

    adding google tags

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,16 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NQ54FV2X"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </Html>
   );
--- pages/index.js
+++ pages/index.js
@@ -63,6 +63,17 @@ export default function Home() {
         <meta property="og:image" content="/favicon.png" />
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Durex Crom" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                      })(window,document,'script','dataLayer','GTM-NQ54FV2X');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </Head>
       <Navigation dataId="navigation" />
       <main className="flex flex-col items-center w-full overflow-hidden">
